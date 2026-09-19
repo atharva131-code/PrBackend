ed3acba3d162512ffda5d90f809745dad1cfa5f4
Atharva Mishra <atharvamishra131@gmail.com>
2026-09-19 13:59:59 +0530

    updated route of coverImage

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -38,7 +38,7 @@ router.route("/current-user").post(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
