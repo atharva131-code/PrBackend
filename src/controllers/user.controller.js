bba0fe8a36a7cc40ed0b35a3201673419c9e8543
Atharva Mishra <atharvamishra131@gmail.com>
2026-09-19 13:45:48 +0530

    add sub pipelines and end points as wll as updated user route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,6 +8,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async(userId)=>{
     try {   
@@ -398,10 +399,56 @@ const getUserChannelProfile = Handler(async(req, res)=>{
     )
 })
 
+const getWatchHistory = Handler(async(req,res)=>{
+   const user = await User.aggregate([
+    {
+        $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup:{
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline: [
+                            {
+                                $project: {
+                                    fullName: 1,
+                                    username : 1,
+                                    avatar: 1
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $first: "$owner"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+   ])
+   return res.status(200)
+   .json(new ApiResponse(200 , user[0].watchHistory, "Watch History fetched Successfully"))
+})
+
 export { registerUser,loginUser,logoutUser,
     refreshAccessToken,changeCurrentPassword,
      getCurrentUser, updateAccountDetails, updateUserAvatar,
-     updateUserCoverImage , getUserChannelProfile
+     updateUserCoverImage , getUserChannelProfile , getWatchHistory
 }
 
 
